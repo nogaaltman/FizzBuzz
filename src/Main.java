63da4846e010bc06a161a1c1f17a4c6e1e32bf16
nogaaltman <noga.altman@mail.utoronto.ca>
2024-09-11 14:39:13 -0400

    mutiples

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        doFizzBuzz(args);
-    }
-
-    private static void doFizzBuzz(String[] args) {
         FizzBuzz.main(args);
     }
 }
---
+++ src/Multiples.java
@@ -0,0 +1,13 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1;
+        while (i < 1000) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum++;
+            }
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
